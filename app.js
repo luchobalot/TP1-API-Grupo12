2d18c8d92ccdb9b04d7d4442e714f4c6c704b1c3
Luciano <lnbalot02@gmail.com>
2024-09-19 14:34:17 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json()); // Para parsear cuerpos de solicitudes JSON
+
+// Rutas (ejemplo)
+app.get('/', (req, res) => {
+  res.send('Funciona correctamente el servidor!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
