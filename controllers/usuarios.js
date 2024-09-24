b42ef0f9e64e610f21ac77fa8b4eea01610bdef6
unknown <agustinpuente90@gmail.com>
2024-09-24 00:21:02 -0300

    getUsers actualizado querys

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,19 +1,50 @@
 const axios =  require('axios');
 
 const getUsers = async (req, res) => {
-  
   try {
     const apiKey = process.env.API_USERS_KEY;
-
+    
+    //querys
+    const { country, gender, first_name, last_name, email } = req.query;
+    
     const response = await axios.get('https://my.api.mockaroo.com/usuarios_api', {
       headers: {
         "X-API-Key": "6ce69550"
-      } 
+      }
     });
-    
-    res.status(200).json({ status: 'ok', data: response.data });
+
+    let users = response.data;
+
+    // Aplicamos los filtros si están presentes
+    if (country) {
+      users = users.filter(user => user.country === country);
+    }
+
+    if (gender) {
+      users = users.filter(user => user.gender === gender);
+    }
+
+    if (first_name) {
+      users = users.filter(user => user.first_name === first_name);
+    }
+
+    if (last_name) {
+      users = users.filter(user => user.last_name === last_name);
+    }
+
+    if (email) {
+      users = users.filter(user => user.email === email);
+    }
+
+    if (users.length > 0) {
+      // Devuelve los usuarios filtrados
+      res.status(200).json({ status: 'ok', data: users });
+    } else {
+      // Si no se encuentran usuarios con los filtros dados, devolver un mensaje
+      res.status(404).json({ status: 'error', message: 'No se encontraron usuarios con esos criterios' });
+    }
   } catch (error) {
-    console.error(error);
+    console.error(error); 
     res.status(500).json({ error: error.message });
   }
 };
